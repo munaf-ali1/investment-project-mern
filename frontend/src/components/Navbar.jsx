5709229e759af5e71330cad820daf74021d343f7
munaf-ali1 <143165045+munaf-ali1@users.noreply.github.com>
2026-01-14 15:17:25 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = ({ setShowWalletModal }) => {
     <div className="flex justify-between items-center px-8 py-4 bg-white shadow">
       
       {/*  LEFT: LOGO */}
-      <h1 className="text-xl font-bold text-indigo-600">
+      <h1 className=" hidden sm:block text-xl font-bold text-indigo-600">
         InvestPro
       </h1>
 

923a3931b67bc4652a6328ff11fe5407d178d0de
munaf-ali1 <143165045+munaf-ali1@users.noreply.github.com>
2026-01-14 11:02:07 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ console.log('Starting server...');
 const port = process.env.PORT || 8000;
 
 app.use(cors({
-    origin:["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"],
+    origin:"https://investment-project.onrender.com",
     credentials:true
 }))
 

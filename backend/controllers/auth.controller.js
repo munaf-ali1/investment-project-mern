6e9d261e6044b8fd0ba0ee00ce6a11a0762d0bd7
munaf-ali1 <143165045+munaf-ali1@users.noreply.github.com>
2026-01-14 14:55:42 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -44,8 +44,8 @@ export const register = async (req, res) => {
     
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "lax",
-      secure: false, // localhost
+      sameSite: "none",
+      secure: true, 
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -81,8 +81,8 @@ export const login = async (req, res) => {
    
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "lax",
-      secure: false, // localhost
+      sameSite: "none",
+      secure: true, 
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
 
